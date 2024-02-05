221f639a9c0b6f31f29c389bc00a248bb26908e8
Joao Couto <jpcouto@gmail.com>
2024-02-04 20:47:47 -0800

    Added general schema

---
+++ src/labdata/schema/general.py
@@ -0,0 +1,233 @@
+from ..utils import *
+import datajoint as dj
+
+#with dj.conn().transaction
+
+if 'database' in prefs.keys():
+    for key in prefs['database'].keys():
+        if prefs['database'][key] is None:
+            if key in ['database.user', 'database.password']:
+                import getpass
+                # get the password and write to file
+                prefs['database'][key] = getpass.getpass(prompt=key)
+                gotpass = True
+        if not prefs['database'][key] is None:
+            dj.config[key] = prefs['database'][key]
+    if 'gotpass' in dir():
+        save_labdata_preferences(prefs, LABDATA_FILE)                
+
+dataschema = dj.schema(dj.config['database.name'])
+
+@dataschema 
+class Files(dj.Manual):
+    definition = '''
+    file_path :            varchar(300)  # Path to the file
+    storage:               varchar(12)   # storage name 
+    ---
+    file_datetime:         datetime      # date created
+    file_size:             double        # using double because int64 does not exist
+    file_md5 = NULL:       varchar(32)   # md5 checksum
+    '''
+    
+@dataschema 
+class Upload(dj.Manual):  # add stuff to the upload table that the computer should upload. this should
+    definition = '''
+    src_path :            varchar(300)  # local file path 
+    upload_storage:       varchar(12)   # storage name
+    upload_host:          varchar(24)   # name of the host computer that should upload
+    ---
+    src_datetime:         datetime      # date created
+    src_size:             double        # using double because int64 does not exist
+    checksum_md5 = NULL:  varchar(32)   # md5 checksum
+    '''
+    
+@dataschema
+class LabMember(dj.Manual):
+    definition = """
+    user_name:		                varchar(32)	# username
+    ---
+    email=null:		                varchar(128)	# email address
+    first_name=null:                    varchar(32)	# first name
+    last_name=null:	                varchar(32)   	# last name
+    date_joined:	                datetime        # when the user joined the lab
+    is_active = 1:                      boolean	        # active or left the lab
+    """
+
+@dataschema
+class Species(dj.Lookup):
+    definition = """
+    species_name              : varchar(32)       # species nickname
+    ---
+    species_scientific_name   : varchar(56)	  # scientific name 
+    species_description=null  : varchar(256)       # description
+    """
+    
+@dataschema
+class Strain(dj.Lookup):
+    definition = """
+    strain_name                : varchar(32)	# strain name
+    ---
+    -> Species
+    strain_description=null    : varchar(256)	# description
+    strain_ts=CURRENT_TIMESTAMP: timestamp
+    """
+
+@dataschema
+class Subject(dj.Manual):
+    ''' Experimental subject.'''
+    definition = """
+    subject_name  : varchar(20)          # unique mouse id
+    ---
+    subject_dob       : date                 # mouse date of birth
+    subject_gender    : enum('M', 'F', 'U')  # sex of mouse - Male, Female, or Unknown
+    -> Strain
+    -> LabMember
+    """
+
+@dataschema
+class SetupLocation(dj.Lookup):
+    definition = """
+    setup_location    : varchar(255) 
+    ---
+"""
+    contents = zip(['CHS-74100'])
+
+@dataschema
+class Setup(dj.Lookup):
+    definition = """
+    setup_name        : varchar(54)     # setup name          
+    ---
+    -> SetupLocation 
+    setup_description : varchar(512) 
+"""
+
+@dataschema
+class Session(dj.Manual):
+    definition = """
+    -> Subject
+    session                  : varchar(54)     # session identifier
+    ---
+    session_datetime         : datetime        # experiment date
+    -> DatasetType 
+    -> [nullable] LabMember 
+    -> [nullable] Setup
+    -> [nullable] Note
+    """
+
+@dataschema
+class DatasetType(dj.Lookup):
+    definition = """
+    dataset_type: varchar(32)
+    """
+    contents = zip(['task-training',
+                    'task-behavior',
+                    'free-behavior',
+                    'imaging-2p',
+                    'imaging-widefield',
+                    'ephys',
+                    'opto-inactivation',
+                    'opto-activation',
+                    'analysis'])
+
+@dataschema
+class Dataset(dj.Manual):
+    definition = """
+    -> Subject
+    -> Session
+    -> DatasetType
+    ---
+    session_datetime         : datetime        # experiment date
+    -> DatasetType 
+    -> [nullable] LabMember 
+    -> [nullable] Setup
+    -> [nullable] Note
+    """
+    class DataFiles(dj.Part):
+        definition = '''
+        -> master
+        -> Files
+        '''        
+    
+@schema
+class Death(dj.Manual):
+    definition = """
+    -> Subject
+    ---
+    death_date:                  date       # death date
+    death_ts=CURRENT_TIMESTAMP:  timestamp
+    -> [nullable] Note
+    """
+
+@dataschema
+class Note(dj.Manual):
+    definition = """
+    -> LabMember
+    -> note_datetime    : data
+    ---
+    notes = ''   : varchar(4000)   # free-text notes
+    """
+    class Image(dj.Part):
+        definition = """
+        -> Note
+        image_id       : int
+        ---
+        image          :  longblob
+        caption = NULL : varchar(256)
+        """
+        
+@dataschema
+class ProcedureType(dj.Lookup):
+    definition = """
+    procedure_type : varchar(52)       #  Defines procedures that are not an experimental session
+    """
+    contents = zip(['implant',
+                    'injection',
+                    'window replacement',
+                    'handling',
+                    'training',
+                    'craniotomy'])
+    
+@dataschema
+class Procedure(dj.Manual):
+    ''' Surgical or behavioral manipulation. '''
+    definition = """
+    -> Subject
+    -> ProcedureType
+    procedure_date         : date                    # date
+    ---
+    -> LabMember
+    procedure_metadata     : longblob        # other
+    animal_weight=null     : decimal(3,1)    # (g) 
+    -> [nullable] Note
+    """
+
+@dataschema
+class Weighing(dj.Manual):
+    definition = """
+    -> Subject
+    weighing_datetime : datetime
+    ---
+    weight : float  # (g)
+    """
+
+@dataschema
+class Watering(dj.Manual):
+    definition = """
+    -> Subject
+    watering_datetime : datetime
+    ---
+    water_volume : float  # (uL)
+    """
+
+    
+@dataschema
+class WaterRestriction(dj.Manual):
+    definition = """
+    -> Subject
+    water_restriction_start_date : date
+    ---
+    -> LabMember
+    water_restriction_end_date : date
+   -> Weighing
+    """
+
